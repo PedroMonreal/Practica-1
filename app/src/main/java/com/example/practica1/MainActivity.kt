994c1afa9f9eed87e7d3b79a6f653f78875fd8f4
PedroMonreal <pedromonrealhernandez@gmail.com>
2025-09-03 01:00:55 -0600

    Se agregaron los textos para el nombre del alumno y para el número de control, también se modificó el tamaño de ambos textos, y se cambió el color de la letra del nombre del alumno

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practica1.ui.theme.Practica1Theme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +29,7 @@ class MainActivity : ComponentActivity() {
             Practica1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Pedro Monreal",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +40,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    /*Text(
+        text =  "$name!",
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment= Alignment.CenterHorizontally
+    )*/
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = name,
+            fontSize = 35.sp,
+            color = Color.Red)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "22130532",
+            fontSize = 25.sp)
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Practica1Theme {
-        Greeting("Android")
+        Greeting("Pedro Monreal")
     }
 }
